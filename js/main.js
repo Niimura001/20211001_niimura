858a80f7f245d5a395c7c20e2bf60fed8589b9a1
Niimura001 <88579417+Niimura001@users.noreply.github.com>
2021-10-06 08:15:30 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-a
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  const target = document.getElementById("menu");
+  target.classList.toggle('open');
+  const nav = document.getElementById("drawer-nav");
+  nav.classList.toggle('in');
+});
